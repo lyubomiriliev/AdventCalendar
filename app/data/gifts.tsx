111e349d8809fb00928efdc198309930ae3b5398
Lyubomir Iliev <iliev.lyubomir98@gmail.com>
2024-12-11 21:59:47 +0200

    adding gifts

---
+++ app/data/gifts.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import {
+  Gift as GiftIcon,
+  Star as StarIcon,
+  Trees as TreeIcon,
+} from "lucide-react";
+import { Gift as GiftType } from "../types";
+
+export const gifts: GiftType[] = [
+  {
+    id: 1,
+    content: "Вечерен масаж на краченца - дълъг с крем",
+    icon: <GiftIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 2,
+    content: "Украсяване на елха заедно с много любов",
+    icon: <TreeIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 3,
+    content: "Подаръкът ти е скрит на любимото ти място. Потърси го.",
+    icon: <GiftIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 4,
+    content: "Turkish Dizi по избор",
+    icon: <StarIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 5,
+    content: "Дълго милване преди заспиване",
+    icon: <GiftIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 6,
+    content: "Много те обичам, коти. Време е за коледен базар!",
+    icon: <TreeIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 7,
+    content: "Дълго TikTok гледане заедно вечерта",
+    icon: <GiftIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 8,
+    content: "Масаж на вратлето и милване",
+    icon: <GiftIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 9,
+    content: "Apple устройства detailing",
+    icon: <StarIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 10,
+    content:
+      "Ти си най-прекрасната жена и никога няма да спра да те обичам. Всеки ден ме правиш все по-щастлив <3 Ciela Giftcard 50BGN",
+    icon: <GiftIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 11,
+    content: "Тирамису by chef Iliev",
+    icon: <GiftIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 12,
+    content: "Масаж на дупенце",
+    icon: <StarIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 13,
+    content: "Коледни филми и гушане",
+    icon: <StarIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 14,
+    content: "Starbucks Date + Mall Shopping",
+    icon: <StarIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 15,
+    content: "Брънч и разходка",
+    icon: <GiftIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 16,
+    content: "Зненадка от chef Iliev",
+    icon: <GiftIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 17,
+    content: "Вечеря на ресторант зненадка",
+    icon: <TreeIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 18,
+    content: "Закуска в леглото",
+    icon: <StarIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 19,
+    content: "Казвам 'ДА' на всичко днес",
+    icon: <GiftIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 20,
+    content: "Изненада! Този път ти подаряваш нещо на мен :)",
+    icon: <StarIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 21,
+    content: "Целодневно мързелуване заедно в леглото",
+    icon: <GiftIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 22,
+    content: "Предколедно Кино(дано да има филм)",
+    icon: <TreeIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 23,
+    content: "Почти е Коледаа. Обичам те <3 Искам да сме завинаги заедно <3",
+    icon: <TreeIcon className="w-12 h-12 text-white" />,
+  },
+  {
+    id: 24,
+    content: "Весела Коледа, Коти <3 Подаръкът ти е в колата ми obviously...",
+    icon: <TreeIcon className="w-12 h-12 text-white" />,
+  },
+];
